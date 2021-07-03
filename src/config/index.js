84106379e8f46691f1e08b9b4620ba4a7dc7f2f7
Jorge Romero <Jorgeddrv@gmail.com>
2021-07-03 13:52:55 -0400

    added(env-config): Prod environment name was modified

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const environmentUrls = new Map();
 
 environmentUrls.set('localhost', 'http://localhost:8080');
-environmentUrls.set('course-book-store', 'https://course-book-store.herokuapp.com');
+environmentUrls.set('course-book-store.herokuapp.com', 'https://course-book-store.herokuapp.com');
 
 export default environmentUrls.get(window.location.hostname);
